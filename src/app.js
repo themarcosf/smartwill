59663c82750f8fe49ac7a8db8886dfa9d470dcba
mflorencio-inteli <marcos.silva@sou.inteli.edu.br>
2022-07-27 15:29:20 -0300

    mongo db setup and post route creation

--- src/app.js
+++ src/app.js
@@ -1,23 +1,31 @@
+//core module
 const path = require("path");
+//npm modules
 const express = require("express");
 const hbs = require("hbs");
+//project module
+require("./db/mongoose");
+const User = require("./models/user");
 
+//setup server
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Define paths for Express config
-const publicDirectoryPath = path.join(__dirname, "../public");
-const viewsPath = path.join(__dirname, "../templates/views");
-const partialsPath = path.join(__dirname, "../templates/partials");
-
-// Setup handlebars engine and views location
+//setup handlebars engine and views location
 app.set("view engine", "hbs");
+const viewsPath = path.join(__dirname, "../templates/views");
 app.set("views", viewsPath);
+const partialsPath = path.join(__dirname, "../templates/partials");
 hbs.registerPartials(partialsPath);
 
-// Setup static directory to serve
+//setup JSON parser
+app.use(express.json());
+
+//setup static directory to serve
+const publicDirectoryPath = path.join(__dirname, "../public");
 app.use(express.static(publicDirectoryPath));
 
+//GET requests
 app.get("", (req, res) => {
   res.render("index", {
     title: "smartWill",
@@ -52,6 +60,19 @@ app.get("*", (req, res) => {
     name: "3A1,5M Tech Ltda.",
     errorMessage: "Página não encontrada.",
   });
+});
+
+//POST request
+app.post("/contato", (req, res) => {
+  const user = new User(req.body);
+  user
+    .save()
+    .then((result) => {
+      res.send(user);
+    })
+    .catch((err) => {
+      res.status(400).send(err);
+    });
 });
 
 app.listen(port, () => {
---
+++ src/db/mongoose.js
@@ -0,0 +1,5 @@
+//npm module
+const mongoose = require("mongoose");
+
+//connect to database
+mongoose.connect("mongodb://127.0.0.1:27017/smartwill");
